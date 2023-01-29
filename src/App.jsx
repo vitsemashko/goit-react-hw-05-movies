b7274ad9f144ad17d281d0a1a8310353100259c0
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2023-01-29 11:00:34 +0200

    Update App.jsx
    1) Added lazy loading and Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
-// import { lazy } from "react";
-import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import { ToastContainer } from "react-toastify";
-import "../node_modules/react-toastify/dist/ReactToastify.minimal.css";
-
-// const Home = lazy(() => import("./pages/Home/Home"));
-// const Movies = lazy(() => import("./pages/Movies/Movies"));
-// const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
-// const MovieDetails = lazy(() => import("./pages/MovieDetails/MovieDetails"));
-// const Cast = lazy(() => import("./components/Cast/Cast"));
-// const Reviews = lazy(() => import("./components/Reviews/Reviews"));
-import SharedLayout from "./components/SharedLayout/SharedLayout";
-import Home from "./pages/Home/Home";
-import Movies from "./pages/Movies/Movies";
-import NotFound from "./pages/NotFound/NotFound";
-import MovieDetails from "./pages/MovieDetails/MovieDetails";
-import Cast from "./components/Cast/Cast";
-import Reviews from "./components/Reviews/Reviews";
-import "./App.css";
+import { ToastContainer } from 'react-toastify';
+import '../node_modules/react-toastify/dist/ReactToastify.minimal.css';
+import './App.css';
+const Home = lazy(() => import('./pages/Home/Home'));
+const Movies = lazy(() => import('./pages/Movies/Movies'));
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
+const MovieDetails = lazy(() => import('./pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./components/Cast/Cast'));
+const Reviews = lazy(() => import('./components/Reviews/Reviews'));
+const SharedLayout = lazy(() =>
+  import('./components/SharedLayout/SharedLayout')
+);
 
 const App = () => {
-	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<SharedLayout />}>
-					<Route index element={<Home />} />
-					<Route path="movies" element={<Movies />} />
-					<Route path="movies/:movieId" element={<MovieDetails />}>
-						<Route path="cast" element={<Cast />} />
-						<Route path="reviews" element={<Reviews />} />
-					</Route>
-				</Route>
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-			<ToastContainer />
-		</div>
-	);
+  return (
+    <div>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+
+      <ToastContainer />
+    </div>
+  );
 };
 export default App;
