7bc416cc47af3b598f416c5804834ac668af4498
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2023-02-08 17:31:16 +0200

    Updated
    1) Added prop-types to deps;
    2) added proptype for query in SearchBox component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,49 +1,50 @@
-import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
-import { FaSearch } from "react-icons/fa";
-import css from "./SearchBox.module.css";
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import css from './SearchBox.module.css';
 
-const SearchBox = ({ query = "", onChange }) => {
-	const [searchQuery, setSearchQuery] = useState("");
+const SearchBox = ({ query = '', onChange }) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-	useEffect(() => {
-		if (query) {
-			setSearchQuery(query);
-		}
-	}, [query]);
+  useEffect(() => {
+    if (query) {
+      setSearchQuery(query);
+    }
+  }, [query]);
 
-	const handleChange = (e) => {
-		setSearchQuery(e.target.value);
-	};
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		onChange(searchQuery);
-		setSearchQuery("");
-	};
+  const handleSubmit = e => {
+    e.preventDefault();
+    onChange(searchQuery);
+    setSearchQuery('');
+  };
 
-	return (
-		<div className={css.Wrapper}>
-			<form className={css.Form} onSubmit={handleSubmit}>
-				<button className={css.Button} type="submit">
-					<FaSearch />
-				</button>
-				<input
-					className={css.Input}
-					type="text"
-					name="searchQuery"
-					value={searchQuery}
-					placeholder="Search movies"
-					autoComplete="off"
-					autoFocus
-					onChange={handleChange}
-				/>
-			</form>
-		</div>
-	);
+  return (
+    <div className={css.Wrapper}>
+      <form className={css.Form} onSubmit={handleSubmit}>
+        <button className={css.Button} type="submit">
+          <FaSearch />
+        </button>
+        <input
+          className={css.Input}
+          type="text"
+          name="searchQuery"
+          value={searchQuery}
+          placeholder="Search movies"
+          autoComplete="off"
+          autoFocus
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
 };
 
 SearchBox.propTypes = {
-	onChange: PropTypes.func.isRequired,
+  query: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 export default SearchBox;
